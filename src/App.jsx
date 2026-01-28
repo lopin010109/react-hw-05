3abd3cbad588afa849793906b59a080cf19605dd
Joshhsieh <halu_9@hotmail.com>
2026-01-28 11:48:58 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+const { VITE_API_BASE: API_BASE, VITE_API_PATH: API_PATH } = import.meta.env;
+
+function App() {
+  
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
